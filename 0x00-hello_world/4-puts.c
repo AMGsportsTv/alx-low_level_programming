12f2a2ec1dc3d4beb5351d038ec708f659c3f160
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-08 23:35:22 +0000

    programing is a task redone and redone and redone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *Return - Always 0 (success)
+ * main - The puts command prints the results to a standard out with a new line
+ *
+ * Return: Always 0 (success)
  */
-
 int main(void)
-{    
+{
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
