54664a765cc051fadfbbe34c44b12a6cc86615c0
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-08 22:41:55 +0000

    Using the puts command in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - this is a simple c program
+ * return - returns to 'Programming is like building a multilingual puzzle'
+ */
+
+int main (void)
+{    
+	puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
+}
