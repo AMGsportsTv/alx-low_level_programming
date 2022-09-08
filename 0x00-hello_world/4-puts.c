b865f9573b0459e04390692099f5857a2679ec93
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-08 23:47:59 +0000

    Indeed programming is multilingual

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - The puts command prints the results to a standard out with a new line
- *
- * Return: Always 0 (success)
+ *main - The printf command prints long outputs.
+ * 
+ *Return: The puts command prints outputs with a new line.
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
