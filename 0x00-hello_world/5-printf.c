c0d956dd084b92c6c49fed375ef31b31ab9d142e
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-09 01:15:43 +0000

    Editing Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-  *main - The difference between the puts and the printf commands
+  *main - The printf command does not print a new line.
   *
-  *Return: The printf command prints the output without a new line
+  *Return: The printf prints the out without a new line.
   */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
-	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
