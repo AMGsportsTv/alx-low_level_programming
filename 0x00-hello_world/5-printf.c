d238ea062b7abdc80a251952fc49599fa3b207e6
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-09 01:03:15 +0000

    Correction for the printf command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- *main - Differentiating the usage of printf and puts commands.
-
- *Return: The printf command prints the output without a new line.
- */
+  *main - The difference between the puts and the printf commands
+  *
+  *Return: The printf command prints the output without a new line
+  */
 int main(void)
 {
 	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	
 	return (0);
 }
