da5cbecd6cea1b71fcaddbce6fceae568bc8eb67
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-09 00:11:30 +0000

    printing to the standard out with a new line, we use the printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - Differentiating the use of printf and puts commands
+ *Return: The printf prints the output without creating a new line.
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
