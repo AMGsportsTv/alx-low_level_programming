7a281886288cdd6fb2df6b08748e0b857ed67b52
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-11 22:24:54 +0000

    the putchar with lowercases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Prints lowercase alphabets
+ *
+ *Return: Always successful
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++);
+	{
+		putchar(c);
+	}
+	return (0);
+}
+
