4fe7c9463a267cde4a73081a56bdffcdd98fd9de
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-12 00:59:06 +0000

    last digit of

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
     }
     else if (i == 0)
     {
-        printf("last digit of %d is %d and is0\n", n);
+        printf("last digit of %d is %d and is 0\n", n, i);
     }
     else
     {
