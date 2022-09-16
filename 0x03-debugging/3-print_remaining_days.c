d1b124a072218977363ca51f98f10c3dcd5e4c37
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-16 04:47:11 +0000

    betty said yes again

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,13 +10,13 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
-		if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+		if (month >= 3 && day >= 60)
 		{
-			if (month >= 3 && day >= 60)
-			{
-				day++;
-			}
+			day++;
+		}
+		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
@@ -30,6 +30,5 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-	}
 }
 
