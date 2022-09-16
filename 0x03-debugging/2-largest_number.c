db666a06d81a8c5dc02baf42237da5738638cb04
AMGsportsTv <atlaspictures256inc@gmail.com>
2022-09-16 06:08:53 +0000

    betty said yes to Task 2, while gcc has 1 exit status

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,7 +7,7 @@
  *@c: third integer
  *Return: largest number
  */
-int largest_number(int a, int b, int c) 
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
@@ -25,4 +25,3 @@ int largest_number(int a, int b, int c)
 	}
 	return (largest);
 }
-
